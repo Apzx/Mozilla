5d4065ae64bf330fdebdbd1c20091e614c3deac3
Apzx <benjamin.mousseau@gmail.com>
2013-08-30 11:20:19 +0200

    Fixed a lot of crash causes, added some error msg
    also started the scalability side.

--- WebRTC/Automatized diagnostic tool/server.js
+++ WebRTC/Automatized diagnostic tool/server.js
@@ -3,16 +3,27 @@ var http = require('http'),
 	qs = require("querystring"),
 	catalog = {},
 	line = [];
+	bots = {
+		"placeholder": {
+			id: null,
+			timeoutKeepAlive: null,
+		}
+
+	};
 
 http.createServer(function (req, res) {
 	console.log("");
-	console.log("");
-	console.log("");
-	console.log('Request received from url:',req.url);
 	var url_parts = url.parse(req.url, true);
 
 	var key = url_parts.query.key;
 	switch(req.url.split("?")[0]) {
+		/*case '/connect':
+			console.log("Connection request recieved");
+			var botKey = Math.floor(Math.random()*1000000)+1000000;
+			break;*/
+
+
+
 		case '/send_offer':
 			console.log("Offer received");
 			var body = '';
@@ -21,9 +32,9 @@ http.createServer(function (req, res) {
 			});
 			req.on('end', function () {
 				var POST = qs.parse(body);
-				console.log(POST)
 				catalog[POST["key"]] = {};
 				catalog[POST["key"]].offer = {};
+				catalog[POST["key"]].answer = {};
 				catalog[POST["key"]].offer.type = POST["offer[type]"];
 				catalog[POST["key"]].offer.sdp = POST["offer[sdp]"];
 
@@ -39,19 +50,19 @@ http.createServer(function (req, res) {
 		case "/ask_for_offer":
 			console.log("Got asked for offer");
 			var offer = line.shift();
-			if (offer != undefined) {
+			if (offer === undefined) {
+				res.writeHead(200, {'Content-Type': 'text/*'});
+				res.end('callback("Nope.avi")');
+				console.log("Do not have one");
+			}
+			else {
 				console.log("Sending pendingOffer...");
 				res.writeHead(200, {'Content-Type': 'text/*'});
 
 				res.end('callback('+JSON.stringify(offer)+')');
 				console.log("PendingOffer sent");
-				
 			}
-			else {
-				res.writeHead(200, {'Content-Type': 'text/*'});
-				res.end('callback("Nope.avi")');
-				console.log("Asked for offer, do not have one");
-			}
+		 
 			break;
 
 
@@ -64,11 +75,16 @@ http.createServer(function (req, res) {
 			});
 			req.on('end', function () {
 				var POST = qs.parse(body);
-				catalog[POST["key"]].answer = {};
-				catalog[POST["key"]].answer.type = POST["answer[type]"];
-				catalog[POST["key"]].answer.sdp = POST["answer[sdp]"];
-				res.writeHead(200, {'Content-Type': 'text/*'});
-				res.end('ok');
+				if (typeof catalog[POST["key"]].answer == 'undefined') {
+					res.writeHead(200, {'Content-Type': 'text/*'});
+					res.end('error');
+				}
+					else {
+					catalog[POST["key"]].answer.type = POST["answer[type]"];
+					catalog[POST["key"]].answer.sdp = POST["answer[sdp]"];
+					res.writeHead(200, {'Content-Type': 'text/*'});
+					res.end('ok');
+				}
 			});
 			break;
 
@@ -76,23 +92,30 @@ http.createServer(function (req, res) {
 
 		case "/ask_for_answer":
 			console.log("Got asked for answer on key: "+key)
-			if (catalog[key].answer != null) {
-				console.log("Sending pendingAnswer...");
+			if (typeof catalog[key] == 'undefined') {
 				res.writeHead(200, {'Content-Type': 'text/*'});
-				res.end('callback(\''+JSON.stringify(catalog[key].answer)+'\')');
-				console.log("pendingAnswer sent");
+				res.end('callback("reset_please")');
 			}
 			else {
-				res.writeHead(200, {'Content-Type': 'text/*'});
-				res.end('callback("Nope.avi")');
-				console.log("Asked for answer, do not have one");
+				if (isEmpty(catalog[key].answer)) {
+					res.writeHead(200, {'Content-Type': 'text/*'});
+					res.end('callback("Nope.avi")');
+					console.log("Asked for answer, do not have one");
+				}
+				else {
+					console.log("Sending pendingAnswer...");
+					res.writeHead(200, {'Content-Type': 'text/*'});
+					res.end('callback(\''+JSON.stringify(catalog[key].answer)+'\')');
+					console.log("pendingAnswer sent");
+					delete catalog[key];
+				}
 			}
 			break;
 
 
 
 		case "/reset":
-			catalog[key] = null;
+			delete catalog[key];
 			console.log("Reset performed on key: "+key);
 
 			res.writeHead(200, {'Content-Type': 'text/*'});
@@ -100,8 +123,20 @@ http.createServer(function (req, res) {
 			break;
 
 		default:
-			res.end(req.url);
+			console.log('Request received from url:',req.url);
+			res.writeHead(200, {'Content-Type': 'text/*'});
+			res.end("You are not supposed to be here.<br>"+req.url);
+			break;
 	}
 	
 }).listen(8080);
 
+
+
+function isEmpty(obj) {
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
+    }
+    return true;
+}
